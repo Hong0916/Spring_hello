e7ec64c3b2c02473fe7eeb190abc69225f0e12a3
Hong0916 <ekfdkshfwk@naver.com>
2023-07-10 16:39:58 +0900

    DB 이전까지 작성완료

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -13,36 +13,35 @@ import hello.hellospring.service.MemberService;
 
 @Controller
 public class MemberController {
-	
+
 	private final MemberService memberService;
-	
+
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
-	
+
 	@GetMapping(value = "/members/new")
 	public String createForm() {
-		return "members/createMemberFrom";
+		return "members/createMemberForm";
 	}
-	
+
 	@PostMapping(value = "/members/new")
 	public String create(MemberForm form) {
+
 		Member member = new Member();
 		member.setName(form.getName());
-		
+
 		memberService.join(member);
-		
+
 		return "redirect:/";
 	}
-	
+
 	@GetMapping(value = "/members")
 	public String list(Model model) {
 		List<Member> members = memberService.findMembers();
 		model.addAttribute("members", members);
 		return "members/memberList";
 	}
-
-
 
 }
